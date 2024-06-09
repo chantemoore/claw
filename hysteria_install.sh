94d6481bd28afbc78bb3752d4207975b64b0d66e
yuckychu <yuckychu@outlook.com>
2024-06-09 16:30:12 +0800

    update hysteria_install.sh

--- hysteria_install.sh
+++ hysteria_install.sh
@@ -25,7 +25,8 @@ done
 
 if [[ -z "$PASSWORD" ]]; then
   echo 'generate a random password...'
-  PASSWORD="$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 10 | head -n 1)"
+#  PASSWORD="$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 10 | head -n 1)"
+  PASSWORD="Huzaiisacat"
 fi
 
 # generate a self-sign certficate
